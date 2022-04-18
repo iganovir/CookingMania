d048d2b279d8ed90613014a8c886c2f7ab946b4c
Iga Noviyanti <iganoviyanti@gmail.com>
2022-04-18 11:26:07 +0700

    Change Remote

--- app/src/main/java/com/dana/onboardingproject/source/remote/RecipeApi.kt
+++ app/src/main/java/com/dana/onboardingproject/source/remote/RecipeApi.kt
@@ -1,7 +1,6 @@
 package com.dana.onboardingproject.source.remote
 
 /**
- * @author Iga Noviyanti created on 18/04/22 at 11.09
- */
-class RecipeApi {
+* @author Iga Noviyanti created on 18/04/22 at 11.09
+*/class RecipeApi {
 }
